1ba6b819c58fe9f447677c6cac7271b439856719
Ashwin Dhangar <ashwindhangar2142002@gmail.com>
2023-12-29 09:04:36 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>🌟 Welcome Contributors! 🌟</h1>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Changes are coming soon to this repo for your help. Stay tuned! Your part will make a big difference. 🚀💡        </p>
+        <a
+          className="App-link"
+          href="https://github.com/MrAshwin2142/Learn-Reactjs"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
