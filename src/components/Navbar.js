c9e5a75d130907db3a1619116c1a3280c2000d80
Ashwin Dhangar <ashwindhangar2142002@gmail.com>
2023-12-29 09:57:07 +0530

    updated baisc structur

---
+++ src/components/Navbar.js
@@ -0,0 +1,18 @@
+import './components.css'
+const Navbar = () => {
+    return (
+        <div className='navbar'>
+            <div className="logo">
+                <h2>Reactjs</h2>
+            </div>
+            <div className="nav-item">
+                <ul>
+                    <li>Contribute</li>
+                    <li>Issue</li>
+                    <li>Theme</li>
+                </ul>
+            </div>
+        </div>
+    );
+}
+export default Navbar;
