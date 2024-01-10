796028bbc96953e29ade851849d50e4308f0a203
MrAshwin2142 <ashwindhangar2142002@gmail.com>
2024-01-10 18:24:54 +0530

    update home

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,7 @@
 const Card = ({coin}) => {
   return (
     <div className="w-48 mx-6 my-2 border px-6 py-2 rounded-lg">
-      <img  alt="logo" src={coin.image}/>
+      <img  alt="logo" loading="lazy" src={coin.image}/>
       <h1 className="text-xl">{coin.id}</h1>
       <h1 className="text-lg">$ {coin.current_price}</h1>
     </div>
--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,7 @@ const Home = () => {
     return (
         <div className="max-w-3xl mx-auto p-6">
             <div className="mb-8">
-                <h1 className="text-3xl font-bold mb-4">Welcome to Learn React</h1>
+                <h1 className="text-3xl font-bold mb-4">Welcome to Learn Reactjs</h1>
                 <p className="text-gray-300">This app is my ongoing project where I'm learning and implementing new features in React. It's a work-in-progress, and I'm continuously improving it. While the current CSS might not be perfect, I'm focused on adding more features and enhancing its design soon.</p>
             </div>
             <div>
@@ -13,7 +13,7 @@ const Home = () => {
                     <li className="mb-2"><span className="font-bold text-lg">Form Validation:</span> Validate user inputs in various forms across the app.</li>
                 </ul>
             </div>
-            <p className="text-gray-300">Thanks for visiting [Your App Name]. Stay tuned for more updates as I continue to learn and grow this project!</p>
+            <p className="text-gray-300">Thanks for visiting <span>Learn Reactjs</span>. Stay tuned for more updates as I continue to learn and grow this project!</p>
 
         </div>
 
